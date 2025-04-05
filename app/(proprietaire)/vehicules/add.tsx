db5a03e6c76a61aa0576be1eed9065eabca30869
Nazaire02 <nazairendouffou@gmail.com>
2025-04-05 16:28:20 +0000

    feat: pick vehicle's image

--- app/(proprietaire)/vehicules/add.tsx
+++ app/(proprietaire)/vehicules/add.tsx
@@ -1,10 +1,34 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import * as ImagePicker from 'expo-image-picker';
 
 export default function add() {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.headerTitle}>Ajout d'un véhicule</Text>
+      <TouchableOpacity onPress={pickImage} style={styles.imagePicker}>
+        {image ? (
+          <Image source={{ uri: image }} style={styles.profileImage} />
+        ) : (
+          <View style={styles.placeholderImage}>
+            <Text style={{ color: "#999" }}>Ajouter une photo</Text>
+          </View>
+        )}
+      </TouchableOpacity>
       <View style={{paddingHorizontal:12}}>
         <View>
           <Text style={styles.label}>Type de véhicule</Text>
@@ -59,7 +83,7 @@ export default function add() {
           <Text>Enregistrer</Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </ScrollView>
   )
 }
 
@@ -91,6 +115,27 @@ const styles = StyleSheet.create({
     justifyContent:"center",
     alignItems:"center",
     borderRadius:4,
-    marginTop:10
-  }
+    marginVertical:14
+  },
+  imagePicker: {
+    alignItems: 'center',
+    marginBottom: 20
+  },
+  profileImage: {
+    width: 140,
+    height: 140,
+    borderRadius: 70,
+    borderWidth: 1,
+    borderColor: "green"
+  },
+  placeholderImage: {
+    width: 140,
+    height: 140,
+    borderRadius: 70,
+    borderWidth: 2,
+    borderColor: "#ccc",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0"
+  },
 })
